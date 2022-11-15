f76614e517449bd5f637b5825442b9d2fb1ff7d8
qwp_p <70431503+Pan-ShiQi@users.noreply.github.com>
2022-11-15 13:50:46 +0800

    自动获取米游社cookie
    更新最基本的获取

---
+++ cookie.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         一键获取米游社cookie
+// @namespace    https://github.com/Pan-ShiQi/-cookie
+// @version      0.1
+// @description  使用via等浏览器登入后获取
+// @author       qwp-Pan_ShiQi
+// @run-at       document-end
+// @match        https://m.bbs.mihoyo.com/ys/#/home/0
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    javascript:(()=>{prompt('',document.cookie)})();
+
+})();
